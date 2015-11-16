a00e378e7a7c7fee6e8015b87b5f902161f18d40
vagrant <vagrant@homestead>
2015-11-16 01:08:00 +0000

    Modify initial view to Atty. Baligod for Senator

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Atty. Baligod for Senator</div>
             </div>
         </div>
     </body>
